7bfe01a1b95ae256f04367f50d102997be30c278
Nicholas Carsner <nicholascarsner@gmail.com>
2024-02-28 15:14:28 -0600

    Minor updates.

--- fun_with_primes.py
+++ fun_with_primes.py
@@ -6,6 +6,7 @@ def is_prime(n):
             return False
     return True
 
+
 def prime_distances():
     primes = [p for p in range(1, 501) if is_prime(p)]
 
@@ -14,25 +15,13 @@ def prime_distances():
         next_distance = primes[i + 1] - prime if i < len(primes) - 1 else 0
         tot_distance = prev_distance + next_distance
 
-        print(f"{i+1} - Prime: {prime}, Prev: {prev_distance}, Next: {next_distance}, Total: {tot_distance}")
+        print(
+            f"{i+1} - Prime: {prime}, Prev: {prev_distance}, Next: {next_distance}, Total: {tot_distance}"
+        )
 
-prime_distances()
 
+# prime_distances()
 
-
-# def is_prime(n):
-#     if n <= 1:
-#         return False
-#     if n <= 3:
-#         return True
-#     if n % 2 == 0 or n % 3 == 0:
-#         return False
-#     i = 5
-#     while i * i <= n:
-#         if n % i == 0 or n % (i + 2) == 0:
-#             return False
-#         i += 6
-#     return True
 
 def count_primes_up_to_500():
     prime_count = 0
@@ -40,9 +29,28 @@ def count_primes_up_to_500():
         if is_prime(num):
             prime_count += 1
         if num % 10 == 0:
-            start_range = num - 9
-            end_range = num
-            print(f"Range {start_range}-{end_range}: {prime_count} primes")
+            range_start = num - 9
+            range_end = num
+            print(f"Range {range_start}-{range_end}: {prime_count} primes")
             prime_count = 0
 
-count_primes_up_to_500()
+
+# count_primes_up_to_500()
+
+
+def print_incremental_primes(n, increment):
+    for num in range(1, n + 1):
+        if num % increment == 1:
+            primes = []
+            range_start = num
+        if is_prime(num):
+            primes.append(num)
+        if num % increment == 0:
+            # range_start = num - 9
+            range_end = num
+            print(
+                f"Range {range_start}-{range_end}: Primes - {', '.join(map(str,primes))}"
+            )
+
+
+print_incremental_primes(500, 50)
