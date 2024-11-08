f5da8cdbde28023212bee9932605429eed11de85
ncarsner <nicholascarsner@gmail.com>
2024-11-08 10:36:08 -0600

    Directory updates.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def next_nearest(n, r):
@@ -31,3 +32,43 @@ def format_time(milliseconds):
         return f"{seconds}.{milliseconds:03f} seconds"
     else:
         return f"{milliseconds:.3f} milliseconds"
+
+
+
+def fibonacci(n):
+    """Recursive function to calculate Fibonacci numbers."""
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def exponential_time_calculation(n):
+    """Function that increases exponentially in time needed to calculate."""
+    start_time = time.time()
+    result = fibonacci(n)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return result, execution_time
+
+
+# def format_time(milliseconds):
+#     """Function to format time in suitable increments."""
+#     if milliseconds >= 60000:
+#         minutes, seconds = divmod(milliseconds / 1000, 60)
+#         return f"{int(minutes)} minutes {seconds:.2f} seconds"
+#     elif milliseconds >= 1000:
+#         return f"{milliseconds / 1000:.2f} seconds"
+#     else:
+#         return f"{milliseconds:.4f} milliseconds"
+
+
+for i in range(15,50,5):
+    result, execution_time = exponential_time_calculation(i)
+    print(f"Fibonacci({i}) = {result:,} | Execution Time: {format_time(execution_time * 1000)}")
+
+
+n = 45  # Fibonacci sequence number to calculate
+# result, execution_time = exponential_time_calculation(n)
+# print(f"Fibonacci({n}) = {result}")
+# print("Execution Time:", format_time(execution_time * 1000))  # Convert to milliseconds
--- iterators.py
+++ iterators.py
@@ -1,6 +1,7 @@
 iterable_list = [i for i in range(11) if i % 2 == 0]
-iterable_tuple = ('apple', 'banana', 'cherry', 'daikon')
+iterable_tuple = ("apple", "banana", "cherry", "daikon")
 iterable_dict = dict(a="alpha", b="bravo", c="charlie", d="delta", e="echo")
+
 
 def iterable_oceans():
     yield "Arctic"
@@ -9,6 +10,7 @@ def iterable_oceans():
     yield "Pacific"
     yield "Antarctic"
 
-iterable_squares = (x*2 for x in range(10))
+
+iterable_squares = (x * 2 for x in range(10))
 
 next(iterable_list)
