ecdfea1926752b4fb29ca54fee045ecfd3e53182
ncarsner <nicholascarsner@gmail.com>
2024-12-26 14:18:48 -0600

    Minor updates.

--- lambdas.py
+++ lambdas.py
@@ -4,4 +4,11 @@ from string import digits as dg
 otp = lambda length=6: "".join(ch(dg) for _ in range(length))
 
 for i in range(5):
-    print(otp())
+    # print(otp())
+    ...
+
+def otp_func(length=6):
+    otp = "".join(ch(dg) for _ in range(length))
+    return otp
+
+print(otp_func(17))
