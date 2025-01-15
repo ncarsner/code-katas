e394727b0712f67bc3cf66d8505a6c9bb8d21789
Nicholas Carsner <nicholascarsner@gmail.com>
2025-01-15 14:51:25 -0600

    Added phrase control mechanisms.

--- generators/business_name_maker.py
+++ generators/business_name_maker.py
@@ -1,48 +1,82 @@
 import random
 
-word_list = {
-    "a": ["Absolution"],
-    "b": ["Biscuit"],
-    "c": ["Crimson", "Carpaccio"],
-    "d": ["Delinquent"],
+adjectives = {
+    # "a": [],
+    # "b": [],
+    "c": ["Crimson", "Cross-Eyed"],
+    "d": ["Delinquent", "Diversive"],
     "e": ["Excellent", "Effington"],
-    "f": ["Fetid", "Ferrous"],
-    # "g": [''],
-    # "h": [''],
-    # "i": [''],
-    # "j": [''],
-    # "k": [''],
-    "l": ["Libertarian"],
-    # "m": [''],
-    # "n": [''],
-    # "o": [''],
-    # "p": [''],
-    # "q": [''],
+    "f": ["Fetid", "Ferrous", "Foolish"],
+    # "g": [],
+    # "h": [],
+    # "i": [],
+    # "j": [],
+    # "k": [],
+    "l": ["Libertarian", "Leaping"],
+    # "m": [],
+    "n": ["Nefarious"],
+    "o": ["Oblivious"],
+    "p": ["Pernicious"],
+    # "q": [],
     "r": ["Renegade"],
     "s": ["Subversive"],
-    # "t": [''],
-    # "u": [''],
-    # "v": [''],
-    # "w": [''],
-    # "x": [''],
-    # "y": [''],
-    # "z": [''],
+    "t": ["Tranquil", "Topaz"],
+    # "u": [],
+    # "v": [],
+    # "w": [],
+    # "x": [],
+    # "y": [],
+    # "z": [],
 }
 
+nouns = {
+    "a": ["Absolution", "Aficionado", "Amethyst"],
+    "b": ["Biscuit"],
+    "c": ["Carpaccio"],
+    "d": ["Delinquent"],
+    "e": ["Eucharist"],
+    # "f": [],
+    # "g": [],
+    # "h": [],
+    # "i": [],
+    # "j": [],
+    # "k": [],
+    "l": ["Libertarian"],
+    "m": ["Maelstrom"],
+    # "n": [],
+    "o": ["Oblivion"],
+    "p": [],
+    # "q": [],
+    "r": ["Renegade"],
+    # "s": [],
+    "t": ["Topaz"],
+    # "u": [],
+    # "v": [],
+    # "w": [],
+    # "x": [],
+    # "y": [],
+    # "z": [],
+}
 
 def generate_business_name(n):
-    filtered_word_list = {k: v for k, v in word_list.items() if v}
+    if n < 2:
+        raise ValueError("n must be at least 2")
 
-    if n > len(filtered_word_list):
-        raise ValueError("n is larger than the number of available letters")
+    filtered_adjectives = {k: v for k, v in adjectives.items() if v}
+    filtered_nouns = {k: v for k, v in nouns.items() if v}
 
-    selected_keys = random.sample(list(filtered_word_list.keys()), n)
+    if n > len(filtered_adjectives) + 1:
+        raise ValueError("n is larger than the number of available words")
+
+    selected_adjective_keys = random.sample(list(filtered_adjectives.keys()), n - 1)
+    selected_noun_key = random.choice(list(filtered_nouns.keys()))
+
     business_name_parts = [
-        random.choice(filtered_word_list[key]) for key in selected_keys
+        random.choice(filtered_adjectives[key]) for key in selected_adjective_keys
     ]
+    business_name_parts.append(random.choice(filtered_nouns[selected_noun_key]))
 
     return " ".join(business_name_parts)
-
 
 if __name__ == "__main__":
     names = 5
