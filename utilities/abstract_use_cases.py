e366fe379e5e529db265397c28b96b7080d794c8
Nicholas Carsner <nicholascarsner@gmail.com>
2025-02-13 16:21:45 -0600

    Added new `set` object use case.

--- utilities/abstract_use_cases.py
+++ utilities/abstract_use_cases.py
@@ -3,26 +3,28 @@
 import random
 
 """
-Output of the implicit empty None type object - i.e. [None, None, None] - may differ when executing in an interactive environment (Jupyter notebook, Debug console) vs. executing in a script.
+Output of the implicit empty None type object - i.e. [None, None, None]
+may differ when executing in an interactive environment (Jupyter notebook, Debug console)
+vs. executing in a script.
 """
 
 list_of_numbers = [random.randint(1, 20) for _ in range(random.randint(5, 10))]
 
 # Returns iterated objects in list, AND list of None types
-lambda_in_list = lambda object: [print(x) for x in object]
+lambda_in_list = lambda object: [print(x) for x in object]  # noqa: E731
 # Returns iterated objects in list, AND list of None types
-lambda_in_list_or_none = lambda object: [print(i) for i in object] or None
+lambda_in_list_or_none = lambda object: [print(i) for i in object] or None  # noqa: E731
 # Returns only iterated objects in list
-lambda_in_list_and_none = lambda object: [print(i) for i in object] and None
+lambda_in_list_and_none = lambda object: [print(i) for i in object] and None  # noqa: E731
 # Returns only iterated objects in list
-lambda_list_list_explicit_empty = lambda object: (print(*object, sep="\n"))
+lambda_list_list_explicit_empty = lambda object: (print(*object, sep="\n"))  # noqa: E731
 
 # Returns generator object
-lambda_in_tuple = lambda object: (print(x) for x in object)
+lambda_in_tuple = lambda object: (print(x) for x in object)  # noqa: E731
 # Returns generator object
-lambda_in_tuple_or_none = lambda object: (print(x) for x in object) or None
+lambda_in_tuple_or_none = lambda object: (print(x) for x in object) or None  # noqa: E731
 # Returns nothing
-lambda_in_tuple_and_none = lambda object: (print(x) for x in object) and None
+lambda_in_tuple_and_none = lambda object: (print(x) for x in object) and None  # noqa: E731
 
 
 lambda_in_list(list_of_numbers)
@@ -33,3 +35,13 @@ lambda_in_list(list_of_numbers)
 # lambda_in_tuple(list_of_numbers)
 # lambda_in_tuple_or_none(list_of_numbers)
 # lambda_in_tuple_and_none(list_of_numbers)
+
+
+set_of_numbers = {random.randint(1, 20) for _ in range(random.randint(5, 10))}
+selected_numbers = {random.choice(list(set_of_numbers)) for _ in range(random.randint(2, len(set_of_numbers) // 2))}
+
+print(f"{set_of_numbers=}")
+print(f"{selected_numbers=}")
+set_of_numbers -= selected_numbers
+
+print(f"{set_of_numbers=}")
