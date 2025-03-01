533026062b729ab6f2d488326dab39d2969fa29f
Nicholas Carsner <nicholascarsner@gmail.com>
2025-03-01 09:38:01 -0600

    Minor updates.

--- builtin_methods/try_except.py
+++ builtin_methods/try_except.py
@@ -1,8 +1,11 @@
+import random
+
+
 def divide_numbers(a, b):
     try:
         result = a / b
     except ZeroDivisionError as e:
-        print(f"Error: Cannot divide by zero. {e}")
+        print(f"Error: {e}")
         return None
     except TypeError as e:
         print(f"Error: Invalid input type. {e}")
@@ -10,9 +13,10 @@ def divide_numbers(a, b):
     else:
         return result
 
+
 def read_file(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data = file.read()
             return data
     except FileNotFoundError as e:
@@ -22,6 +26,7 @@ def read_file(file_path):
         print(f"Error: IO error occurred. {e}")
         return None
 
+
 def convert_to_int(value):
     try:
         return int(value)
@@ -29,20 +34,24 @@ def convert_to_int(value):
         print(f"Error: Cannot convert to integer. {e}")
         return None
 
+
 def perform_custom_function(func, *args, **kwargs):
     try:
         result = func(*args, **kwargs)
         return result
     except Exception as e:
-        print(f"Error: An error occurred while performing the custom function. {e}")
+        print(f"Error: occurred while performing the custom function. {e}")
         return None
 
 
-print(divide_numbers(10, 2))  # prints: 5.0
-print(divide_numbers(10, 0))  # handles division by zero
-print(divide_numbers(10, 'a'))  # handles type error
+a = random.randint(1, 10)
+b = random.randint(1, 10)
 
-file_path = 'example.txt'
+print(divide_numbers(a, b))
+print(divide_numbers(random.choice([a, b]), 0))  # handles division by zero
+print(divide_numbers(random.choice([a, b]), "a"))  # handles type error
+
+file_path = "example.txt"
 file_content = read_file(file_path)  # handles file not found
 if file_content:
     print(file_content)
@@ -50,12 +59,13 @@ if file_content:
 print(convert_to_int("123"))  # prints: 123
 print(convert_to_int("abc"))  # handles value error
 
-# Custom function
 custom_function = lambda x, y: x + y
-print(perform_custom_function(custom_function, 5, 3))  # prints: 8
+print(perform_custom_function(custom_function, a, b))
+
 
 # Custom function with error
 def faulty_function(x):
     return x / 0
+
 
 print(perform_custom_function(faulty_function, 5))  # handles the division by zero error
