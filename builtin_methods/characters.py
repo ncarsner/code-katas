9323ff6a50eaa990741b09fbca8a878c52f5891d
Nicholas Carsner <nicholascarsner@gmail.com>
2025-03-06 14:27:23 -0600

    Initial commit.

---
+++ builtin_methods/characters.py
@@ -0,0 +1,19 @@
+print("Degree: \N{DEGREE SIGN}")
+print("Copyright: \N{COPYRIGHT SIGN}")
+print("Registered: \N{REGISTERED SIGN}")
+print("Trademark: \N{TRADE MARK SIGN}")
+
+print("\nCurrency: \N{CURRENCY SIGN}")
+print("Euro: \N{EURO SIGN}")
+print("Pound: \N{POUND SIGN}")
+print("Yen: \N{YEN SIGN}")
+
+print("\nSection: \N{SECTION SIGN}")
+print("Paragraph: \N{PILCROW SIGN}")
+print("Micro: \N{MICRO SIGN}")
+print("Infinity: \N{INFINITY}")
+
+print("\nNotEqual: \N{NOT EQUAL TO}")
+print("LessEqual: \N{LESS-THAN OR EQUAL TO}")
+print("GreaterEqual: \N{GREATER-THAN OR EQUAL TO}")
+print("PlusMinus: \N{PLUS-MINUS SIGN}")
